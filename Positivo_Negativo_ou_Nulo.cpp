a9826c1b8321c81ec5ce8a86eb8f2f3f3041f9f9
Reinaehfe <reinaldoferro4@gmail.com>
2025-04-19 21:28:21 -0300

    enviando o código

---
+++ Positivo_Negativo_ou_Nulo.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+	int n;
+	cin >> n;
+
+	if (n > 0){
+		cout << "positivo" << endl;	
+	}else if (n < 0){
+		cout << "negativo" << endl;
+	}else{
+		cout << "nulo" << endl;
+	}
+
+	return 0;
+}
